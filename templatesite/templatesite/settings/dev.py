545ef091aff2974ba2d779745cb95b05d96ad58d
andreobriennz <andre.obrien877@gmail.com>
2022-04-24 12:10:13 +1200

    Create basic Wagtail setup

---
+++ templatesite/templatesite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-pn)f0iy5zppt1fi)xa&)gw3n8p&9+-#xv^cv24oqgq63(jxw+@'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
