10aa19883787eccb6f771aec1fc7588e9a502134
andreobriennz <andre.obrien877@gmail.com>
2022-04-26 18:28:33 +1200

    Add accordion, conditional container, minor typography improvements, and reorder some components in style guide

--- templatesite/templatesite/static/js/components.js
+++ templatesite/templatesite/static/js/components.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const CLOSE_ACCORDIONS_ONCLICK = true
+
 // UTILS
 function el(selector) {
     return document.querySelector(selector)
@@ -40,6 +42,39 @@ window.handleSearch = function(selector) {
     return false
 }
 
+/*
+To use this:
+Add the [data-accordion-item] HTML attribute around the accordion item.
+Then add [data-title] and [data-content] around the title and around the content which will be displayed when the title is clicked.
+*/
+function watchAccordionItems() {
+    const accordionItems = document.querySelectorAll('[data-accordion-item]')
+    accordionItems.forEach(item => {
+        const title = item.querySelector('[data-title]')
+        const content = item.querySelector('[data-content]')
+
+        title.addEventListener('click', () => {
+            CLOSE_ACCORDIONS_ONCLICK && closeOtherAccordions(content)
+
+            toggleClass(content, 'display-none')
+
+            const downArrow = item.querySelector('.icon-down-arrow') // todo: use font awesome
+            toggleClass(downArrow, 'rotate-180')
+        }, { passive: true })
+    })
+}
+
+function closeOtherAccordions(element) {
+    const accordionItems = document.querySelectorAll('[data-accordion-item] [data-content]')
+    accordionItems.forEach(item => {
+        if (element !== item) {
+            item.classList.add('display-none')
+        }
+    })
+}
+
+
+// ONLOAD
 document.addEventListener('DOMContentLoaded', () => {
-    // watchAccordionItems()
+    watchAccordionItems()
 }, false)
